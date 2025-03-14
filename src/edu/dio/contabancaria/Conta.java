3c8933e3a65d3cfe09846d11d2f0b2cb191e39f2
Vini <vini.ba.dev@gmail.com>
2025-03-14 10:36:20 -0300

    feat: adiciona método de imprimir mensagem

--- src/edu/dio/contabancaria/Conta.java
+++ src/edu/dio/contabancaria/Conta.java
@@ -1,8 +1,17 @@
 package edu.dio.contabancaria;
 
+import java.text.MessageFormat;
+
 public class Conta {
     String agencia = "";
     int numero = 0;
     String nomeCliente = "";
     double saldo = 0.00;
+
+    public void imprimeMensagem() {
+        String mensagem = MessageFormat.format(
+                "\n Olá {0}, obrigado por criar uma conta em nosso banco, sua agência é {1}, conta {2} e seu saldo {3} já está disponível para saque\".",
+                nomeCliente, agencia, numero, saldo);
+        System.out.println(mensagem);
+    }
 }
