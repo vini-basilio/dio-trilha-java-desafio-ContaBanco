113f9da3605ca3f3b45ea6a3f6ab74a69875132e
Vini <vini.ba.dev@gmail.com>
2025-03-14 11:05:26 -0300

    feat: adiciona a classe conta ao método main

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import edu.dio.contabancaria.Conta;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
+        var conta = new Conta();
+
+        conta.iniciarScanner();
+
+        conta.inicializarAgencia();
+        conta.inicializarConta();
+        conta.inicializarCliente();
+        conta.inicializarSaldo();
+
+        conta.fecharScanner();
+
+        conta.imprimeMensagem();
     }
 }
