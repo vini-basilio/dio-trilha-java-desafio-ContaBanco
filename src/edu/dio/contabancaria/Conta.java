6df86af9d2d57377df9e973f1d5b4f90a89db539
Vini <vini.ba.dev@gmail.com>
2025-03-14 11:02:16 -0300

    fix: corrige o bug causado pelo scanner
    O bug acontece por dois motivos: falta de consumir o caractere '\n' após os tipos numéricos serem pedidos e destruição inadequada do scanner.

--- src/edu/dio/contabancaria/Conta.java
+++ src/edu/dio/contabancaria/Conta.java
@@ -8,44 +8,50 @@ public class Conta {
     int numero = 0;
     String nomeCliente = "";
     double saldo = 0.00;
+    Scanner scanner;
+
+    public void iniciarScanner() {
+        scanner = new Scanner(System.in);
+    }
 
     public void imprimeMensagem() {
         String mensagem = MessageFormat.format(
-                "\n Olá {0}, obrigado por criar uma conta em nosso banco, sua agência é {1}, conta {2} e seu saldo {3} já está disponível para saque\".",
+                "\nOlá {0}, obrigado por criar uma conta em nosso banco, sua agência é {1}, conta {2} e seu saldo {3} já está disponível para saque\".",
                 nomeCliente, agencia, numero, saldo);
         System.out.println(mensagem);
     }
 
     public void inicializarAgencia() {
-        // Inicia scanner
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("\nPor favor, digite o número da Agência!");
         agencia = scanner.nextLine();
-        // Encerra scanner
-        scanner.close();
+
     }
 
     public void inicializarConta() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Por favor, digite o número da Conta!");
         numero = scanner.nextInt();
-        // Encerra scanner
-        scanner.close();
+        scanner.nextLine();
+
     }
 
     public void inicializarCliente() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Por favor, digite o nome do Cliente!");
         nomeCliente = scanner.nextLine();
-        // Encerra scanner
-        scanner.close();
+
     }
 
     public void inicializarSaldo() {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Por favor, digite o Saldo da Conta!");
         saldo = scanner.nextDouble();
-        // Encerra scanner
+        scanner.nextLine();
+
+    }
+
+    public void fecharScanner() {
         scanner.close();
     }
 
